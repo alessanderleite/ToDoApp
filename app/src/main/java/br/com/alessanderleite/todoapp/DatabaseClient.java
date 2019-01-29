7e636fac307aa6ceb116886d417a4cb5d5b7ccbd
alessanderleite <alessander.logistica@gmail.com>
2019-01-29 16:51:54 -0200

    add code inside the DatabaseClient, variables, constructor and methods

--- app/src/main/java/br/com/alessanderleite/todoapp/DatabaseClient.java
+++ app/src/main/java/br/com/alessanderleite/todoapp/DatabaseClient.java
@@ -1,4 +1,34 @@
 package br.com.alessanderleite.todoapp;
 
+import android.arch.persistence.room.Room;
+import android.content.Context;
+
+import static android.arch.persistence.room.Room.databaseBuilder;
+
 public class DatabaseClient {
+
+    private Context mContext;
+    private static DatabaseClient mInstance;
+
+    //our app database object
+    private AppDatabase appDatabase;
+
+    private DatabaseClient(Context mContext) {
+        this.mContext = mContext;
+
+        //creating the app database with Room database builder]
+        //MyToDos is the name of the database
+        appDatabase = databaseBuilder(mContext, AppDatabase.class, "MyToDos").build();
+    }
+
+    public static synchronized DatabaseClient getInstance(Context mContext) {
+        if (mInstance == null) {
+            mInstance = new DatabaseClient(mContext);
+        }
+        return mInstance;
+    }
+
+    public AppDatabase getAppDatabase() {
+        return appDatabase;
+    }
 }
