fb9084fcc5af5b8a9ff9a11845866c00e8f52b67
alessanderleite <alessander.logistica@gmail.com>
2019-01-30 14:00:44 -0200

    create the updateTask method and an inner class inside it and then call the updateTask inside the onCreate method

--- app/src/main/java/br/com/alessanderleite/todoapp/UpdateTaskActivity.java
+++ app/src/main/java/br/com/alessanderleite/todoapp/UpdateTaskActivity.java
@@ -1,9 +1,13 @@
 package br.com.alessanderleite.todoapp;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class UpdateTaskActivity extends AppCompatActivity {
 
@@ -24,6 +28,14 @@ public class UpdateTaskActivity extends AppCompatActivity {
         final Task task = (Task) getIntent().getSerializableExtra("task");
 
         loadTask(task);
+
+        findViewById(R.id.button_update).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_LONG).show();
+                updateTask(task);
+            }
+        });
     }
 
     private void loadTask(Task task) {
@@ -31,5 +43,52 @@ public class UpdateTaskActivity extends AppCompatActivity {
         editTextDesc.setText(task.getDesc());
         editTextFinishBy.setText(task.getFinishBy());
         checkBoxFinished.setChecked(task.isFinished());
+    }
+
+    private void updateTask(final Task task) {
+        final String sTask = editTextTask.getText().toString().trim();
+        final String sDesc = editTextDesc.getText().toString().trim();
+        final String sFinishBy = editTextFinishBy.getText().toString().trim();
+
+        if (sTask.isEmpty()) {
+            editTextTask.setError("Task required");
+            editTextTask.requestFocus();
+            return;
+        }
+
+        if (sDesc.isEmpty()) {
+            editTextDesc.setError("Desc required");
+            editTextDesc.requestFocus();
+            return;
+        }
+
+        if (sFinishBy.isEmpty()) {
+            editTextFinishBy.setError("Finish by required");
+            editTextFinishBy.requestFocus();
+            return;
+        }
+
+        class UpdateTask extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                task.setTask(sTask);
+                task.setDesc(sDesc);
+                task.setFinishBy(sFinishBy);
+                task.setFinished(checkBoxFinished.isChecked());
+                DatabaseClient.getInstance(getApplicationContext()).getAppDatabase()
+                        .taskDao()
+                        .update(task);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Toast.makeText(getApplicationContext(), "Updated", Toast.LENGTH_LONG).show();
+                finish();
+                startActivity(new Intent(UpdateTaskActivity.this, MainActivity.class));
+            }
+        }
     }
 }
