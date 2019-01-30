ba9183cc4320f4d644129a319ac137bce4718339
alessanderleite <alessander.logistica@gmail.com>
2019-01-30 13:40:20 -0200

    instantiate and initialize the EditText and CheckBox inside UpdateTaskActivity class

--- app/src/main/java/br/com/alessanderleite/todoapp/UpdateTaskActivity.java
+++ app/src/main/java/br/com/alessanderleite/todoapp/UpdateTaskActivity.java
@@ -2,12 +2,27 @@ package br.com.alessanderleite.todoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class UpdateTaskActivity extends AppCompatActivity {
+
+    private EditText editTextTask, editTextDesc, editTextFinishBy;
+    private CheckBox checkBoxFinished;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_task);
+
+        editTextTask = findViewById(R.id.editTextTask);
+        editTextDesc = findViewById(R.id.editTextDesc);
+        editTextFinishBy = findViewById(R.id.editTextFinishBy);
+
+        checkBoxFinished = findViewById(R.id.checkboxFinished);
+
+        final Task task = (Task) getIntent().getSerializableExtra("task");
+
+        loadTask(task);
     }
 }
