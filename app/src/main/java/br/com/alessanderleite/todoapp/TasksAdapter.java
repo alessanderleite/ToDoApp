7e722cc3a244ae15be726f0324f498e37c456421
alessanderleite <alessander.logistica@gmail.com>
2019-01-30 09:22:03 -0200

    code the TasksAdapter class making them extends RecyclerView.Adapter

--- app/src/main/java/br/com/alessanderleite/todoapp/TasksAdapter.java
+++ app/src/main/java/br/com/alessanderleite/todoapp/TasksAdapter.java
@@ -1,4 +1,77 @@
 package br.com.alessanderleite.todoapp;
 
-public class TasksAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHolder> {
+
+    private Context context;
+    List<Task> taskList;
+
+    public TasksAdapter(Context context, List<Task> taskList) {
+        this.context = context;
+        this.taskList = taskList;
+    }
+
+    @NonNull
+    @Override
+    public TasksViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recyclerview_tasks, parent, false);
+        return new TasksViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TasksViewHolder holder, int position) {
+        Task t = taskList.get(position);
+        holder.textViewTask.setText(t.getTask());
+        holder.textViewDesc.setText(t.getDesc());
+        holder.textViewFinishBy.setText(t.getFinishBy());
+
+        if (t.isFinished()) {
+            holder.textViewStatus.setText("Completed");
+        } else {
+            holder.textViewStatus.setText("Not Completed");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskList.size();
+    }
+
+    public class TasksViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView textViewStatus;
+        TextView textViewTask;
+        TextView textViewDesc;
+        TextView textViewFinishBy;
+
+        public TasksViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewStatus = itemView.findViewById(R.id.textViewStatus);
+            textViewTask = itemView.findViewById(R.id.textViewTask);
+            textViewDesc = itemView.findViewById(R.id.textViewDesc);
+            textViewFinishBy = itemView.findViewById(R.id.textViewFinishBy);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Task task = taskList.get(getAdapterPosition());
+
+            Intent intent = new Intent(context, UpdateTaskActivity.class);
+            intent.putExtra("task", task);
+
+            context.startActivity(intent);
+        }
+    }
 }
